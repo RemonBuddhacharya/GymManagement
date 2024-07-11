1a2cad2b5343b5eb5f3791564c1658d131eba2e3
RemonBuddhacharya <remonshakya1@gmail.com>
2024-07-11 22:33:43 +0545

    s

--- database/New.sql
+++ database/New.sql
@@ -31,7 +31,14 @@ CREATE TABLE IF NOT EXISTS `gymapp` (
   PRIMARY KEY (`contact`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table gymwebsite.gymapp: ~12 rows (approximately)
+INSERT INTO `gymapp` (`fname`, `lname`, `email`, `contact`, `trainer_id`) VALUES
+	('ram', 'thapa', 'ramthapa@gamil.com', '103', '102'),
+	('bhabishya', 'thapa', 'bhabishyabhattnepal@gmail.com', '109', '1001'),
+	('Saswot', 'b', 'bs@gmail.com', '110', '1001'),
+	('RAM', 'RAM', '1231@GMAIL.COM', '111', '1001'),
+	('Raj', 'kumar', 'kumar@gmail.com', '201', '101'),
+	('Aadarsh', 'thakur', 'thakur@gmail.com', '205', '103');
 
 -- Dumping structure for table gymwebsite.logintb
 DROP TABLE IF EXISTS `logintb`;
@@ -40,7 +47,9 @@ CREATE TABLE IF NOT EXISTS `logintb` (
   `password` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table gymwebsite.logintb: ~0 rows (approximately)
+INSERT INTO `logintb` (`username`, `password`) VALUES
+	('admin', 'pass');
 
 -- Dumping structure for table gymwebsite.package
 DROP TABLE IF EXISTS `package`;
@@ -51,7 +60,11 @@ CREATE TABLE IF NOT EXISTS `package` (
   PRIMARY KEY (`Package_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table gymwebsite.package: ~3 rows (approximately)
+INSERT INTO `package` (`Package_id`, `Package_name`, `Amount`) VALUES
+	('121', 'preliminary', 800),
+	('122', 'Wt. gain', 1500),
+	('123', 'Wt.loss', 1000);
 
 -- Dumping structure for table gymwebsite.payment
 DROP TABLE IF EXISTS `payment`;
@@ -63,7 +76,14 @@ CREATE TABLE IF NOT EXISTS `payment` (
   PRIMARY KEY (`Payment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table gymwebsite.payment: ~4 rows (approximately)
+INSERT INTO `payment` (`Payment_id`, `Amount`, `customer_id`, `payment_type`) VALUES
+	(301, 1500, '201', 'cash'),
+	(302, 800, '202', 'card'),
+	(303, 1000, '203', 'cheque'),
+	(304, 1500, '204', 'cash'),
+	(305, 5000, '10', 'cash'),
+	(306, 1500, '104', 'cash');
 
 -- Dumping structure for table gymwebsite.trainer
 DROP TABLE IF EXISTS `trainer`;
@@ -74,7 +94,9 @@ CREATE TABLE IF NOT EXISTS `trainer` (
   PRIMARY KEY (`Trainer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table gymwebsite.trainer: ~4 rows (approximately)
+INSERT INTO `trainer` (`Trainer_id`, `Name`, `phone`) VALUES
+	(1001, 'Rijan', 659450);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
